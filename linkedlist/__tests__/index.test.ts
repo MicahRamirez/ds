c33e85b298deddc5a8545d90ba04931653eb9c7b
MicahRamirez <xaviermicahramirez@utexas.edu>
2023-01-09 16:39:23 +0900

    testing iterability

--- linkedlist/__tests__/index.test.ts
+++ linkedlist/__tests__/index.test.ts
@@ -6,8 +6,55 @@ describe("LinkedList", () => {
       const ll = new LinkedList();
 
       expect(ll.size()).toBe(0);
-      expect(ll.head).toBe(null);
-      expect(ll.tail).toBe(null);
+      expect(ll.getHead().value).toBe(null);
+      expect(ll.getTail().value).toBe(null);
     });
   });
+
+  describe("iterability", () => {
+    it("should not iterate once with only the sentinel node", () => {
+      const ll = new LinkedList();
+      let iterCount = 0;
+      for (let i of ll) {
+        iterCount += 1;
+      }
+      expect(iterCount).toBe(0);
+    });
+
+    it("should iterate once per element pushed", () => {
+      const ll = new LinkedList();
+      let iterCount = 0;
+
+      ll.add(1);
+      ll.add(2);
+      for (let _i of ll) {
+        iterCount += 1;
+      }
+      expect(iterCount).toBe(2);
+    });
+  });
+
+  describe("add", () => {
+    it("should add a value to the end of the list", () => {
+      const ll = new LinkedList();
+      ll.add(1);
+
+      expect(ll.size()).toBe(1);
+      expect(ll.getHead().next.value).toBe(1);
+      expect(ll.getTail().value).toBe(1);
+    });
+
+    it("should add multiple values to the end of the list", () => {
+      const ll = new LinkedList();
+      ll.add(1);
+      ll.add(2);
+      ll.add(3);
+
+      expect(ll.size()).toBe(3);
+      let index = 0;
+      const head = ll.getHead();
+    });
+  });
+
+  describe("addAtIndex", () => {});
 });
--- linkedlist/index.ts
+++ linkedlist/index.ts
@@ -8,15 +8,32 @@ class Node<T> {
   }
 }
 
-class LinkedList<T> {
-  public head: Node<T> = null;
-  public tail: Node<T> = null;
+// Access	Search	Insertion	Deletion
+// O(n)	O(n)	O(1)	O(n)
+/**
+ * SinglyLinkedList
+ */
+class LinkedList<T> implements Iterable<T> {
+  private head: Node<T>;
+  private tail: Node<T>;
   public length: number = 0;
-  constructor() {}
+  constructor() {
+    const headNode = new Node(null, null);
+    this.head = headNode;
+    this.tail = headNode;
+  }
 
-  add(value: T) {}
+  /**
+   * should be a O(1) op
+   */
+  add(value: T) {
+    const newNode = new Node(value, null);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
+  }
 
-  add(index: number, value: T) {}
+  addAtIndex(index: number, value: T) {}
 
   addAll(ll: LinkedList<T>) {}
 
@@ -25,6 +42,14 @@ class LinkedList<T> {
   addLast(value: T) {}
 
   get(index: number) {}
+
+  getHead(): Node<T> {
+    return this.head;
+  }
+
+  getTail(): Node<T> {
+    return this.tail;
+  }
 
   removeFirst(): T {}
 
@@ -41,6 +66,27 @@ class LinkedList<T> {
   #insertIndexRecur(index: number, value: T) {}
 
   #insertIndexIter(index: number, value: T) {}
+  [Symbol.iterator]() {
+    const length = this.length;
+    let it = this.head;
+    // TODO: What is the expectation on LL iterator with zero length. A little confused with this implementation
+    // because it immediately iterates with value done true and value of
+    // cases length 0
+    // h -> dummy -/>
+    // t -> dummy -/>
+
+    return {
+      next() {
+        it = it.next;
+        const isDone = length === 0 || it === null;
+
+        return {
+          done: isDone,
+          value: it?.value,
+        };
+      },
+    };
+  }
 }
 
 export default LinkedList;
